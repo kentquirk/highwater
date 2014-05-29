3bcec1a6d5f814a9ca3f5961ee164e30e0fa283f
kentquirk <kentquirk@gmail.com>
2014-05-28 23:47:09 -0400

    Initial commit of highwater; so far, it posts but it doesn't serve.

---
+++ highwater.go
@@ -0,0 +1,62 @@
+// Highwater is a server that directs content to the kissmetrics metrics site
+package highwater
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"sync"
+)
+
+type highwater struct {
+	host   string
+	apikey string
+	salt   string
+}
+
+var instance *highwater = nil
+
+func Init(host, apikey, salt string) {
+	var once sync.Once
+	once.Do(func() { instance = &highwater{host, apikey, salt} })
+}
+
+func saveMetrics(user, event string, parms url.Values) {
+	metricsUrl, err := url.Parse(instance.host)
+	if err != nil {
+		log.Fatal(err)
+	}
+	vals := make(url.Values)
+	for k, v := range parms {
+		vals[k] = v
+	}
+	vals.Add("_p", user)
+	vals.Add("_k", instance.apikey)
+	vals.Add("_n", event)
+	metricsUrl.RawQuery = vals.Encode()
+
+	mu := metricsUrl.String()
+	_, err = http.Get(mu)
+	log.Println(mu)
+	if err != nil {
+		log.Println(err) // we'll log the error but won't get upset about it for metrics
+	}
+}
+
+// hash_id generates the same values as the hash function in the javascript highwater.
+func hash_id(id string, length int) string {
+	hash := sha1.New()
+	io.WriteString(hash, instance.salt)
+	io.WriteString(hash, id)
+	result := hash.Sum(nil)
+	out := fmt.Sprintf("%x", result)
+	return out[:length]
+}
+
+// NamedUser creates a log entry with a hashed key, given a userid
+func NamedUser(userid, event string, parms url.Values) {
+	saveMetrics(hash_id(userid, 10), event, parms)
+}
---
+++ highwater_test.go
@@ -0,0 +1,21 @@
+package highwater
+
+import (
+	"net/url"
+	"testing"
+)
+
+func TestHighwater(t *testing.T) {
+	Init("http://trk.kissmetrics.com/e",
+		"28814957ee8160309f522a0bd0f2824de585befb",
+		"gf78fSEI7tOQQP9xfXMO9HfRyMnW4Sx88Q",
+	)
+	v := url.Values{}
+	v.Add("file", "highwater_test.go")
+	ch := make(chan int)
+	go func() {
+		NamedUser("b@b.com", "Go Highwater Test", v)
+		ch <- 1
+	}()
+	<-ch
+}
